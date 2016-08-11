80ba10ca66777c49a8c4e6b0a0d558b827da2b8d
Gmail <uxawseny@gmail.com>
2016-08-11 21:30:48 +0800

    创建项目

---
+++ mysite/src/com/mysite/servlet/registerServlet.java
@@ -0,0 +1,33 @@
+package com.mysite.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * registerServlet
+ */
+@WebServlet("/registerServlet")
+public class registerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		request.setCharacterEncoding("UTF-8");
+		String username=request.getParameter("username");
+		if(username.equalsIgnoreCase("xuwei")){
+			response.getWriter().print("1");
+		}else if(!username.equalsIgnoreCase("")){
+			response.getWriter().print("0");
+		}else{
+			response.getWriter().print("null");
+		}
+	}
+
+}
